432ddc61d75a8c7eed23df8c69cb5421299eaca7
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-11 07:15:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://sahu27:sahu@registration-api.x80arum.mongodb.net/Registration-API?retryWrites=true&w=majority")
     .then(() => {
         console.log('MongoDB connected');
         app.listen(process.env.PORT || port, () => {
